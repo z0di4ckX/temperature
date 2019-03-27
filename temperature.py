92efaa4bdf99f0937ba2cfeef39d293f0b004bdb
Wesly J. Hernandez <ultimatecracker88@gmail.com>
2019-03-27 00:25:02 -0400

    Calculate temperature

---
+++ temperature.py
@@ -0,0 +1,20 @@
+temperature=[10,-20,-289,100]
+def c_to_f(c):
+    if c > -273.15:
+        f=c*9/5+32
+        return f
+    else:
+        return "That temperature dosen't sense!"
+    for t in temperature:
+        print(c_to_f(t))
+        
+temperature=[10,-20,-289.100]
+def c_to_f(c):
+    if c > -273.15:
+        return "That temeprature dosen't sense!"
+    else:
+        f=c*9/5+32
+        return f
+    for t in temperature:
+        print(c_to_f)
+        
